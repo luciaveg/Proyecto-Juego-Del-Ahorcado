3d173bb6cb08d038cb62a84ca705b782f3e92a2d
Lucía vega <luciavf98@gmail.com>
2023-10-30 19:25:30 +0100

    empezado codigo de javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+const palabras = [
+  "ordenador",
+  "codigo",
+  "flor",
+  "delfin",
+  "television",
+  "carpeta",
+  "ciudad",
+  "reloj",
+];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const numeroRandom = getRandomInt(0, 7);
+console.log(numeroRandom);
+const palabraAleatoria = palabras[numeroRandom];
+console.log(palabraAleatoria);
+
+let adivinaPalabra = [];
+
+let divide = palabraAleatoria.split("");
+console.log(divide);
